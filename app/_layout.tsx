e08c2c401f8195a5ddf0425af1146c8b6b528450
narwinoks <narnowin195@gmail.com>
2025-11-02 12:11:30 +0700

    core(app): setup font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,38 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useEffect } from 'react'; // <--- Import useEffect
+import { useColorScheme } from 'react-native';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
 import "../styles/global.css";
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [fontsLoaded, error] = useFonts({
+    'Inter': require('../assets/fonts/Inter-VariableFont.ttf'),
+    'Lexend': require('../assets/fonts/Lexend-VariableFont.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'Raleway-Italic': require('../assets/fonts/Raleway-Italic-VariableFont.ttf'),
+    'Raleway': require('../assets/fonts/Raleway-VariableFont.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
